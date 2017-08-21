2ab5616c93603e08b438603f6b0cfae7acdc92bc
whileD <whiled.tech@gmail.com>
2017-08-22 03:14:19 +0900

    models/user.goのUser修正

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 type User struct {
-	Id   int
-	Name string
+	Id       int
+	Name     string `orm:"size(64);unique"`
+	Email    string `orm:"size(64);unique"`
+	Password string `orm:"size(64)"`
 }
