292b991f8b1e2cacf22b45749f66696a76f357a3
whileD <whiled.tech@gmail.com>
2017-09-26 01:19:39 +0900

    storeに追加できるようにした

--- static/js/task.js
+++ static/js/task.js
@@ -4,21 +4,33 @@ const store = new Vuex.Store({
     },
 
     mutations :{
-        initTasks(state, data) {
-            state.tasks = data
-        }
+        addTask(state, data){
+            state.tasks.push(data)
+        },
     }
 })
 
 let todoNewTask = Vue.extend({
-    
     template: '<div> new task </div>'
 })
 
 
 
 let todoTasks = Vue.extend({
-    template: '<div> task list </div>'
+    created: function() {
+        axios.post('/task/all')
+            .then(response => {
+                response.data.forEach(task => store.commit('addTask', task))
+            })
+    },
+
+    computed : {
+        tasks() {
+            return store.state.tasks
+        }
+    },
+
+    template: temp,
 })
 
 /*
