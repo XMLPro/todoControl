bb0f73cedcd06ac028d7acea9ae821f5e58bf61a
whileD <whiled.tech@gmail.com>
2017-10-03 00:21:55 +0900

    beegoをやめた

--- models/init.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import (
-	"github.com/astaxie/beego/orm"
-)
-
-func init() {
-	orm.RegisterModel(new(User), new(Todo), new(PlaceImp))
-}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	Id       int
-	Name     string `orm:"size(64);unique"`
-	Email    string `orm:"size(64);unique"`
-	Password string `orm:"size(64)"`
-}
